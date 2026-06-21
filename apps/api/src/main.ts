88acb9de9431337cf3dce08553061c3a8ea35861
Pawan <pawannannaware16@gmail.com>
2026-06-21 12:21:30 +0530

    feat: initialize NestJS application configurations and bootstrap code in apps/api

---
+++ apps/api/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  // Set up port from env or default to 3001
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
+}
+bootstrap();
